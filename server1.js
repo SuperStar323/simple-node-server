302296a8d40ad61ac602f9b91f3798ae8afcb4ba
✨aTosinu✨ <atosinu@gmail.com>
2022-07-16 14:08:54 +0900

    [Update] Ping pong

--- server1.js
+++ server1.js
@@ -4,9 +4,34 @@ const port = 3000;
 const express = require('express')
 const app = express()
 
-app.get('/ping', function(req, res) {
-    res.send('pong\n')
+app.get('/ping', function (req, res) {
+  res.send('pong\n')
 })
+
+const ping = () => {
+  const http = require('http');
+  const options = {
+    hostname: 'localhost',
+    port: 5000,
+    path: '/ping',
+    method: 'GET',
+  };
+
+  const req = http.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    res.on('data', d => {
+      process.stdout.write(d);
+    });
+  });
+  req.on('error', error => {
+    console.error(error);
+  });
+  req.end();
+}
+
+setInterval(() => {
+  ping();
+}, 5000);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
