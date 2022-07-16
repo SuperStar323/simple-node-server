844934fa2d9116aaf68f9717b81c40545ea8ebef
✨aTosinu✨ <atosinu@gmail.com>
2022-07-16 14:03:57 +0900

    [Update] Simple node server

---
+++ server1.js
@@ -0,0 +1,13 @@
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express')
+const app = express()
+
+app.get('/ping', function(req, res) {
+    res.send('pong\n')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
